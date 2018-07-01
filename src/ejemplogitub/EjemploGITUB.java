a9fa0ab0acca95df5fc92e822b982dcec16d606d
alexander <denizamora7@gmail.com>
2018-07-01 12:14:45 -0500

    mensaje: xd

---
+++ src/ejemplogitub/EjemploGITUB.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogitub;
+
+/**
+ *
+ * @author deniz
+ */
+public class EjemploGITUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("archivo subido a github xd");
+    }
+    
+}
